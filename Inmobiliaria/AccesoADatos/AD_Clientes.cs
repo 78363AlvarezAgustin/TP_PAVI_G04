c0d4d4ed07e742ceae81747f5ab58c457f75eca7
74047-Gabriela-Quiroga <galequiroga97@gmail.com>
2021-05-30 22:07:06 -0300

    Clientes por Propiedades

--- Inmobiliaria/AccesoADatos/AD_Clientes.cs
+++ Inmobiliaria/AccesoADatos/AD_Clientes.cs
@@ -42,6 +42,88 @@ namespace Inmobiliaria.AccesoADatos
             {
                 cn.Close();
             }
+
         }
+
+
+        public static DataTable ObtenerClientes()
+        {
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBD"];
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+
+                string consulta = "GetClientes";
+
+                cmd.Parameters.Clear();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = consulta;
+
+
+                cn.Open();
+                cmd.Connection = cn;
+
+                DataTable tabla = new DataTable();
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(tabla);
+
+                return tabla;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+
+            }
+        }
+
+        public static DataTable ObtenerTabla(string nombreTabla)
+        {
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBD"];
+
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+
+            try
+            {
+
+                SqlCommand cmd = new SqlCommand();
+
+                string consulta = "SELECT * FROM " + nombreTabla;
+
+                cmd.Parameters.Clear();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+
+                DataTable tabla = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(tabla);
+
+
+
+                return tabla;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+
     }
 }
--- Inmobiliaria/Formularios/FrmTransacciones/frmRegistrarPropiedadInteresParaClientes.cs
+++ Inmobiliaria/Formularios/FrmTransacciones/frmRegistrarPropiedadInteresParaClientes.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Inmobiliaria.AccesoADatos;
+using Inmobiliaria.AccesoADatos.Transacciones;
+using Inmobiliaria.Entidades;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +18,138 @@ namespace Inmobiliaria.Formularios.FrmTransacciones
         public frmRegistrarPropiedadInteresParaClientes()
         {
             InitializeComponent();
+            CargarGrillaPropiedades();
+            CargarComboDocumentos();
+            CargarGrillaClientes();
+        }
+
+        private void btnLimpiarCampos_Click(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+        }
+
+        private void LimpiarCampos()
+        {
+            txtDesignacionCatastral.Text = "";
+            txtNroDocumento.Text = "";
+            cmbTipoDocumento.SelectedItem = null;
+        }
+
+        private void CargarGrillaPropiedades()
+        {
+            try
+            {
+                gdrPropiedades.DataSource = AD_Propiedades.ObtenerPropiedades();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error al cargar la grilla!");
+                throw;
+            }
+        }
+
+        private void CargarGrillaClientes()
+        {
+            try
+            {
+                gdrClientes.DataSource = AD_Clientes.ObtenerClientes();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Error al Obtener Clientes");
+                throw;
+            }
+        }
+
+        private void CargarComboDocumentos()
+        {
+            try
+            {
+
+                cmbTipoDocumento.DataSource = AD_Clientes.ObtenerTabla("tipos_documentos"); //lo naranja es a que tabla tengo que ir a buscar los datos tipo doc
+                cmbTipoDocumento.DisplayMember = "n_tipo_documento"; //lo que quiero mostrar de la bd
+                cmbTipoDocumento.ValueMember = "id_tipo_documento"; //lo que quiero guardar en otro lado
+                cmbTipoDocumento.SelectedIndex = -1;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+        
+        
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (cmbTipoDocumento.SelectedItem == null || txtDesignacionCatastral.Text.Equals("") || txtNroDocumento.Text.Equals(""))
+            {
+                MessageBox.Show("Debe Cargar Todos los Campos");
+            }
+            else
+            {
+                gdrClientes_X_Propiedad.Rows.Add(txtDesignacionCatastral.Text, txtNroDocumento.Text, cmbTipoDocumento.Text);
+                btnGuardar.Enabled = true;
+
+            }
+        }
+
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            bool res = AD_PropiedadInteresClientes.AgregarClientesXPropiedades(int.Parse(txtNroDocumento.Text), cmbTipoDocumento.SelectedIndex, int.Parse(txtDesignacionCatastral.Text));
+
+            if (res)
+            {
+                MessageBox.Show("Registro de Propiedades de interes para Clientes realizado con Exito");
+            }
+            else
+            {
+                MessageBox.Show("Error al Registrar Propiedades de interes para Clientes");
+            }
+
+            gdrClientes_X_Propiedad.Rows.Clear();
+            gdrClientes_X_Propiedad.Refresh();
+            btnGuardar.Enabled = false;
+
+        }
+
+        
+        private void gdrClientes_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                int indice = e.RowIndex;
+                DataGridViewRow filaSeleccionada = gdrClientes.Rows[indice];
+                txtNroDocumento.Text = filaSeleccionada.Cells["Ndoc"].Value.ToString();
+                cmbTipoDocumento.SelectedIndex = (int)filaSeleccionada.Cells["tDoc"].Value; 
+
+            }
+
+            catch (Exception)
+            {
+                MessageBox.Show("Algo ha salido mal, por favor seleccione una fila");
+            }
+
+        }
+
+        private void gdrPropiedades_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int indice = e.RowIndex;
+            DataGridViewRow filaSeleccionda = gdrPropiedades.Rows[indice];
+            txtDesignacionCatastral.Text = filaSeleccionda.Cells["DesignacionCatastral"].Value.ToString();
+        }
+
+
+        private void btnCancelar_Click_1(object sender, EventArgs e)
+        {
+            DialogResult aviso = MessageBox.Show("¿Esta seguro que desea cancelar la transacción?", "Confirmar salida", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (aviso == DialogResult.Yes)
+            {
+                this.Close();
+            }
         }
     }
 }
