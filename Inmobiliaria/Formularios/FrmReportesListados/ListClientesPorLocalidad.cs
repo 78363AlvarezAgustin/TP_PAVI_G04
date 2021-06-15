9f2df5e6785cdea3fc830b0e5753da3e753a129e
Rodrigo Virgolini <rodrigovirgolini@gmail.com>
2021-06-15 20:00:34 -0300

    Reportes Completo

--- Inmobiliaria/Formularios/FrmReportesListados/ListClientesPorLocalidad.cs
+++ Inmobiliaria/Formularios/FrmReportesListados/ListClientesPorLocalidad.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Inmobiliaria.AccesoADatos;
+using Microsoft.Reporting.WinForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,54 @@ namespace Inmobiliaria.Formularios.FrmReportesListados
         public ListClientesPorLocalidad()
         {
             InitializeComponent();
+        }
+
+        private void ListClientesPorLocalidad_Load(object sender, EventArgs e)
+        {
+            this.reportViewer1.RefreshReport();
+            DataTable tabla = new DataTable();
+            tabla = AD_Clientes.ObtenerClientePorLocalidad();
+
+            ReportDataSource ds = new ReportDataSource("DatosClientesPorLocalidad", tabla);
+
+            reportViewer1.LocalReport.DataSources.Clear();
+            reportViewer1.LocalReport.DataSources.Add(ds);
+            reportViewer1.RefreshReport();
+            //reportViewer1.LocalReport.Refresh();
+        }
+
+        private void btnFiltrarDni_Click(object sender, EventArgs e)
+        {
+            this.reportViewer1.RefreshReport();
+            DataTable tabla = new DataTable();
+            tabla = AD_Clientes.ObtenerClienteYDesigCatastralPorDocumento(txtDni.Text);
+
+            ReportDataSource ds = new ReportDataSource("DatosClientesPorLocalidad", tabla);
+
+            reportViewer1.LocalReport.DataSources.Clear();
+            reportViewer1.LocalReport.DataSources.Add(ds);
+            reportViewer1.RefreshReport();
+        }
+
+        private void btnFiltrarDes_Click(object sender, EventArgs e)
+        {
+            this.reportViewer1.RefreshReport();
+            DataTable tabla = new DataTable();
+            tabla = AD_Clientes.ObtenerClienteYDesigCatastralPorDocumento(txtDni.Text);
+
+            ReportDataSource ds = new ReportDataSource("DatosClientesPorLocalidad", tabla);
+
+            reportViewer1.LocalReport.DataSources.Clear();
+            reportViewer1.LocalReport.DataSources.Add(ds);
+            reportViewer1.RefreshReport();
+
+        }
+
+        private void btnLimpiarFiltros_Click(object sender, EventArgs e)
+        {
+            txtDes.Text = "";
+            txtDni.Text = "";
+            ListClientesPorLocalidad_Load(sender, e);
         }
     }
 }
--- Inmobiliaria/Formularios/FrmReportesListados/ListDeEdificiosPorBarrio.cs
+++ Inmobiliaria/Formularios/FrmReportesListados/ListDeEdificiosPorBarrio.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Inmobiliaria.AccesoADatos;
+using Microsoft.Reporting.WinForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +18,54 @@ namespace Inmobiliaria.Formularios.FrmReportesListados
         {
             InitializeComponent();
         }
+
+        private void ListDeEdificiosPorBarrio_Load(object sender, EventArgs e)
+        {
+            this.reportViewer1.RefreshReport();
+            DataTable tabla = new DataTable();
+            tabla = AccesoADatos.AD_Edificios.ObtenerTabla("edificios");
+
+            ReportDataSource ds = new ReportDataSource("DatosBarrios", tabla);
+
+            reportViewer1.LocalReport.DataSources.Clear();
+            reportViewer1.LocalReport.DataSources.Add(ds);
+            reportViewer1.RefreshReport();
+            //reportViewer1.LocalReport.Refresh();        }
+        }
+
+        private void btnFiltrarDni_Click(object sender, EventArgs e)
+        {
+            this.reportViewer1.RefreshReport();
+            DataTable tabla = new DataTable();
+            tabla = AD_Edificios.ObtenerEdificioPorNombre(txtNombre.Text);
+
+            ReportDataSource ds = new ReportDataSource("DatosBarrios", tabla);
+
+            reportViewer1.LocalReport.DataSources.Clear();
+            reportViewer1.LocalReport.DataSources.Add(ds);
+            reportViewer1.RefreshReport();
+        }
+
+        private void btnFiltrarDes_Click(object sender, EventArgs e)
+        {
+            this.reportViewer1.RefreshReport();
+            DataTable tabla = new DataTable();
+            tabla = AD_Edificios.ObtenerEdificioPorBarrio(txtBarrio.Text);
+
+            ReportDataSource ds = new ReportDataSource("DatosBarrios", tabla);
+
+            reportViewer1.LocalReport.DataSources.Clear();
+            reportViewer1.LocalReport.DataSources.Add(ds);
+            reportViewer1.RefreshReport();
+        }
+
+        private void btnLimpiarFiltros_Click(object sender, EventArgs e)
+        {
+            txtBarrio.Text = " ";
+            txtNombre.Text = "";
+            ListDeEdificiosPorBarrio_Load(sender, e);
+
+        }
     }
 }
+
