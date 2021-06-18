fc8dc8ecdc9dd465a0ed458f8f6aba93acce7ddd
77002-Lautaro-SantaCruz <sclautaroo@gmail.com>
2021-06-18 20:24:49 -0300

    Estadistica Empleados por Barrio

--- Inmobiliaria/Formularios/FrmEstadisticas/frmEstadisticaEmpleadosPorBarrio.Designer.cs
+++ Inmobiliaria/Formularios/FrmEstadisticas/frmEstadisticaEmpleadosPorBarrio.Designer.cs
@@ -29,20 +29,77 @@ namespace Inmobiliaria.Formularios.FrmEstadisticas
         /// </summary>
         private void InitializeComponent()
         {
+            this.reportViewer3 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbBarrio = new System.Windows.Forms.ComboBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // reportViewer3
+            // 
+            this.reportViewer3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.reportViewer3.LocalReport.ReportEmbeddedResource = "Inmobiliaria.EstadisticasPorEmpleado.rdlc";
+            this.reportViewer3.Location = new System.Drawing.Point(2, 53);
+            this.reportViewer3.Name = "reportViewer3";
+            this.reportViewer3.ServerReport.BearerToken = null;
+            this.reportViewer3.Size = new System.Drawing.Size(663, 541);
+            this.reportViewer3.TabIndex = 0;
+            this.reportViewer3.Load += new System.EventHandler(this.reportViewer1_Load);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(98, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Empleados del Barrio:";
+            // 
+            // cmbBarrio
+            // 
+            this.cmbBarrio.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBarrio.FormattingEnabled = true;
+            this.cmbBarrio.Location = new System.Drawing.Point(251, 12);
+            this.cmbBarrio.Name = "cmbBarrio";
+            this.cmbBarrio.Size = new System.Drawing.Size(160, 21);
+            this.cmbBarrio.TabIndex = 32;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(418, 12);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 33;
+            this.btnBuscar.Text = "Filtrar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
             // 
             // frmEstadisticaEmpleadosPorBarrio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(663, 592);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.cmbBarrio);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.reportViewer3);
             this.Name = "frmEstadisticaEmpleadosPorBarrio";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "frmEstadisticaEmpleadosPorBarrio";
+            this.Text = "Cantidad Empleados por Barrio";
+            this.Load += new System.EventHandler(this.frmEstadisticaEmpleadosPorBarrio_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer3;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.ComboBox cmbBarrio;
+        private System.Windows.Forms.Button btnBuscar;
     }
 }
