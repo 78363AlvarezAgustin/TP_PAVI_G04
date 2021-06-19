3a0cdec888269f89d49643ef0a673deb7f663c5c
Rodrigo Virgolini <rodrigovirgolini@gmail.com>
2021-06-19 12:34:59 -0300

    Agregado filtro a estadistica

--- Inmobiliaria/Formularios/FrmEstadisticas/frmEstadisticaImporteTotalGastadoEnPublicidPorIdDeMedioPublicitario.Designer.cs
+++ Inmobiliaria/Formularios/FrmEstadisticas/frmEstadisticaImporteTotalGastadoEnPublicidPorIdDeMedioPublicitario.Designer.cs
@@ -30,35 +30,73 @@ namespace Inmobiliaria.Formularios.FrmEstadisticas
         private void InitializeComponent()
         {
             this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.cmbBarrio = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // reportViewer1
             // 
-            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.reportViewer1.LocalReport.ReportEmbeddedResource = "Inmobiliaria.EstadisticaTotalImporteXMedioPublic.rdlc";
-            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Location = new System.Drawing.Point(31, 76);
             this.reportViewer1.Name = "reportViewer1";
             this.reportViewer1.ServerReport.BearerToken = null;
-            this.reportViewer1.Size = new System.Drawing.Size(800, 450);
+            this.reportViewer1.Size = new System.Drawing.Size(757, 431);
             this.reportViewer1.TabIndex = 0;
             this.reportViewer1.Load += new System.EventHandler(this.reportViewer1_Load);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(488, 30);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 36;
+            this.btnBuscar.Text = "Filtrar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // cmbBarrio
+            // 
+            this.cmbBarrio.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBarrio.FormattingEnabled = true;
+            this.cmbBarrio.Location = new System.Drawing.Point(321, 30);
+            this.cmbBarrio.Name = "cmbBarrio";
+            this.cmbBarrio.Size = new System.Drawing.Size(160, 21);
+            this.cmbBarrio.TabIndex = 35;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(102, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 17);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "Categoria de seccion publicitaria";
             // 
             // frmEstadisticaImporteTotalGastadoEnPublicidPorIdDeMedioPublicitario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 519);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.cmbBarrio);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.reportViewer1);
             this.Name = "frmEstadisticaImporteTotalGastadoEnPublicidPorIdDeMedioPublicitario";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Porcentaje de importe gastado en publicidad por Medio Publicitario";
             this.Load += new System.EventHandler(this.frmEstadisticaImporteTotalGastadoEnPublicidPorIdDeMedioPublicitario_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.Button btnBuscar;
+        public System.Windows.Forms.ComboBox cmbBarrio;
+        private System.Windows.Forms.Label label1;
     }
 }
